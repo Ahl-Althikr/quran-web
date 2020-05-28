48207beec2df742a9b43439a54b1323d2faf8124
Abdelrahmam Salem <abubakir1997@gmail.com>
2020-05-28 15:59:10 -0400

    Initial setup with sample code

---
+++ src/stores/store.js
@@ -0,0 +1,10 @@
+import { gettersPlugin } from '@anew/plugins'
+import Store from '@anew/store'
+
+import counter from './counter/counter'
+
+export default new Store({
+  modules: { counter },
+
+  plugins: [gettersPlugin],
+})
