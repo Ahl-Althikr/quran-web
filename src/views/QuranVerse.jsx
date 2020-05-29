6fbd2cd2c39dc621b1e61e3b29db8acbcc4fb197
Abdelrahmam Salem <abubakir1997@gmail.com>
2020-05-28 21:31:42 -0400

    Decreasing height/width/font-size

--- src/views/QuranVerse.jsx
+++ src/views/QuranVerse.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import ClassNames from './QuranVerse.module.scss'
+
 export default class QuranVerse extends React.Component {
   render() {
     const { verse } = this.props
 
     return (
-      <span>
+      <span className={ClassNames.QuranVerse}>
         {verse.arabic_unicodes.map((arabic_unicode) => (
           <span key={arabic_unicode}>{arabic_unicode}</span>
         ))}
