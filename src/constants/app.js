de61046248bcf8d505a812e7278fdff337e6664d
Abdelrahmam Salem <abubakir1997@gmail.com>
2020-05-29 02:43:56 -0400

    Adding chapter title where appropiate

--- src/constants/app.js
+++ src/constants/app.js
@@ -7,3 +7,24 @@ export const JSON_URL =
 
 export const FONT_URL =
   'https://raw.githubusercontent.com/Ahl-Althikr/scripts/master/quran_fonts_data/quran_page_{}.ttf'
+
+export const NEXT_CHAPTER_TITLE = new Set([
+  3,
+  9,
+  21,
+  22,
+  23,
+  25,
+  26,
+  32,
+  33,
+  36,
+  37,
+  44,
+  46,
+  52,
+  59,
+  63,
+  64,
+  79,
+])
--- src/views/QuranVerse.jsx
+++ src/views/QuranVerse.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
 
+import { NEXT_CHAPTER_TITLE } from 'constants/app'
 import ClassNames from './QuranVerse.module.scss'
+import { last } from 'utils/array'
 
 export default class QuranVerse extends React.Component {
   render() {
     const { verse, chapters } = this.props
+
     const chapter = chapters[verse.chapter]
+    const nextChapter = chapters[chapter.number + 1]
+
+    const lastChapterVerseId = last(chapter.verses)
+    const includeNextChapterTitle = NEXT_CHAPTER_TITLE.has(chapter.number)
+    const includedInPrevChapterTitle = NEXT_CHAPTER_TITLE.has(
+      chapter.number - 1
+    )
 
     return (
       <>
         {verse.number === 1 && (
           <>
-            <div className={ClassNames.QuranChapterTitle}>
-              <div className={ClassNames.QuranChapter}>
-                {chapter.arabic_unicode}
+            {!includedInPrevChapterTitle && (
+              <div className={ClassNames.QuranChapterTitle}>
+                <div className={ClassNames.QuranChapter}>
+                  {chapter.arabic_unicode}
+                </div>
               </div>
-            </div>
+            )}
             {chapter.basmalah && (
               <div className={ClassNames.QuranBasmalah}>﷽</div>
             )}
@@ -26,6 +38,13 @@ export default class QuranVerse extends React.Component {
             <span key={arabic_unicode}>{arabic_unicode}</span>
           ))}
         </span>
+        {includeNextChapterTitle && verse.id === lastChapterVerseId && (
+          <div className={ClassNames.QuranChapterTitle}>
+            <div className={ClassNames.QuranChapter}>
+              {nextChapter.arabic_unicode}
+            </div>
+          </div>
+        )}
       </>
     )
   }
